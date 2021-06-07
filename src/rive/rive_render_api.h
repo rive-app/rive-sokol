ecd28741f9fd47220bed0c5089742708db603a2c
Jhonny Göransson <jhonny.goransson@gmail.com>
2021-06-07 15:22:34 +0200

    Remove unused

--- src/rive/rive_render_api.h
+++ src/rive/rive_render_api.h
@@ -56,13 +56,6 @@ namespace rive
         uint32_t          m_IsClipping       : 1;
     };
 
-    struct CreateRendererParams
-    {
-        RenderMode      m_RenderMode;
-        RequestBufferCb m_RequestBufferCb;
-        DestroyBufferCb m_DestroyBufferCb;
-    };
-
     struct DrawBuffers
     {
         union
